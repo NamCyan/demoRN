a3cade4eddaa0e402b4e32a43df3759eb9d6616e
Le Hai Nam <nam.lehai10@mobifone.vn>
2022-03-09 15:31:46 +0700

    init

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import CatListItems from './components/CatList'
+// import cat1 from "../assets/cat.png"
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <CatListItems name= "mèo đen" imagesrc= {require("./assets/cat.png")}/>
+      <CatListItems name= "kronol" imagesrc= {require("./assets/cat2.png")}/>
+      <CatListItems name= "mèo ngơ" imagesrc= {require("./assets/cat3.png")}/>
     </View>
   );
 }
@@ -13,8 +17,10 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    alignItems: 'stretch',
+    backgroundColor: 'pink',
     justifyContent: 'center',
+    paddingRight: 10,
+    paddingLeft: 10
   },
 });
---
+++ components/CatList.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react'
+import { Button, View, Text, Image, StyleSheet, TextInput } from 'react-native'
+import { BoolifiedDeprecatedTextStylePropTypes } from 'react-native/Libraries/Components/View/ReactNativeStyleAttributes';
+
+export default function CatListItems(props) {
+    const [islove, setislove] = useState(false);
+
+    return( 
+        <View style= {styles.container}>
+            <Text style= {styles.title}>{props.name}</Text>
+            <Image source= {props.imagesrc} style={styles.catimage} />
+        </View> 
+    )
+}
+
+const styles = StyleSheet.create(
+    {
+        catimage: {
+            width: 100,
+            height: 100,
+        },
+        container: {
+            padding: 16,
+            backgroundColor: "powderblue",
+            alignItems: 'center',
+            justifyContent: 'center',
+            borderRadius: 1,
+            shadowOpacity: 0.2,
+            shadowRadius: 10,
+            shadowOffset: { width:0, height:0},
+            marginBottom: 8,
+        },
+        title: {
+            textTransform: 'uppercase',
+            marginBottom: 16,
+            fontWeight: "900"
+        }
+    }
+)
